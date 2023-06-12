5026e86c97337b4baae3117557deb15bdcfbb8b7
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-12 09:37:21 +0530

    tip

---
+++ Tip/tip.js
@@ -0,0 +1,26 @@
+
+
+// querySelector 
+// querySelectorAll
+
+// by Tag Name , by Class Name , by ID
+
+
+
+// by ID : 
+// document.getElementById("id");
+
+
+// by Class Name :
+// document.getElementsByClassName("class");
+
+// by Tag Name :
+// document.getElementsByTagName("tag");
+
+
+// The table with id="age-table".
+// All label elements inside that table (there should be 3 of them).
+// The first td in that table (with the word “Age”).
+// The form with name="search".
+// The first input in that form.
+// The last input in that form.
